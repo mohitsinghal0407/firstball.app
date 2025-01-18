f1dc70d48b52ea7474c42019a685097069a3f187
w-udhav <udhavwadhawan@hotmail.com>
2025-01-19 00:32:04 +0530

    login with phone number working

--- src/config/index.js
+++ src/config/index.js
@@ -1,49 +1,54 @@
 const production = false;
-const appVersion = "v9";
-const appName = "FirstBall";
+const appVersion = 'v9';
+const appName = 'FirstBall';
 
-const siteUrl = "https://firstball.in";
-const liveHost = "https://api.firstball.in/api/v1";
-const testHost = "https://api.firstball.in/api/v1";
-const playStoreUrl = "https://play.google.com/store/apps/details?id=com.firstballapp";
+const siteUrl = 'https://firstball.in';
+const liveHost = 'https://api.firstball.in/api/v1';
+const testHost = 'https://api.firstball.in/api/v1';
+const playStoreUrl =
+  'https://play.google.com/store/apps/details?id=com.firstballapp';
 // const testHost = "http://localhost:3000/api/v1";
 
 const livekitServerUrl = 'wss://stream.firstball.in';
 
 const errorMessage = {
-	NOT_FOUND: 'NOT_FOUND',
-	FORBIDDEN: 'FORBIDDEN',
-	PAYMENT_INTENT_ID_IS_NOT_VALID: 'PAYMENT_INTENT_ID_IS_NOT_VALID',
-	UNAUTHENTICATED: 'UNAUTHENTICATED',
-	UNAUTHORIZED: 'UNAUTHORIZED',
-	MISSING_ACCESS_TOKEN: 'MISSING_ACCESS_TOKEN',
-	MALFORMED_ACCESS_TOKEN: 'MALFORMED_ACCESS_TOKEN',
-	EXPIRED_ACCESS_TOKEN: 'EXPIRED_ACCESS_TOKEN',
-	INVALID_EMAIL_OR_PASSWORD: 'INVALID_EMAIL_OR_PASSWORD',
-	EMAIL_IS_ALREADY_TAKEN: 'EMAIL_IS_ALREADY_TAKEN',
-	PASSWORD_IS_NOT_VALID: 'PASSWORD_IS_NOT_VALID',
-	PLAN_ALREADY_HAS_FEEDBACK: 'PLAN_ALREADY_HAS_FEEDBACK',
-	PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_FIRST_DAY_OF_CYCLE: 'PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_FIRST_DAY_OF_CYCLE',
-	NO_PRODUCT_FOUND_BY_NAME: 'NO_PRODUCT_FOUND_BY_NAME',
-	REVEAL_ALREADY_HAS_FEEDBACK: 'REVEAL_ALREADY_HAS_FEEDBACK',
-	PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_DUE_DATE: 'PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_DUE_DATE',
-	MISSING_CLIENT_SECRET_IN_PAYMENT_INTENT: 'MISSING_CLIENT_SECRET_IN_PAYMENT_INTENT',
-}
+  NOT_FOUND: 'NOT_FOUND',
+  FORBIDDEN: 'FORBIDDEN',
+  PAYMENT_INTENT_ID_IS_NOT_VALID: 'PAYMENT_INTENT_ID_IS_NOT_VALID',
+  UNAUTHENTICATED: 'UNAUTHENTICATED',
+  UNAUTHORIZED: 'UNAUTHORIZED',
+  MISSING_ACCESS_TOKEN: 'MISSING_ACCESS_TOKEN',
+  MALFORMED_ACCESS_TOKEN: 'MALFORMED_ACCESS_TOKEN',
+  EXPIRED_ACCESS_TOKEN: 'EXPIRED_ACCESS_TOKEN',
+  INVALID_EMAIL_OR_PASSWORD: 'INVALID_EMAIL_OR_PASSWORD',
+  EMAIL_IS_ALREADY_TAKEN: 'EMAIL_IS_ALREADY_TAKEN',
+  PASSWORD_IS_NOT_VALID: 'PASSWORD_IS_NOT_VALID',
+  PLAN_ALREADY_HAS_FEEDBACK: 'PLAN_ALREADY_HAS_FEEDBACK',
+  PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_FIRST_DAY_OF_CYCLE:
+    'PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_FIRST_DAY_OF_CYCLE',
+  NO_PRODUCT_FOUND_BY_NAME: 'NO_PRODUCT_FOUND_BY_NAME',
+  REVEAL_ALREADY_HAS_FEEDBACK: 'REVEAL_ALREADY_HAS_FEEDBACK',
+  PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_DUE_DATE:
+    'PREVIOUS_DATE_OF_BIRTH_IS_LATER_THAN_DUE_DATE',
+  MISSING_CLIENT_SECRET_IN_PAYMENT_INTENT:
+    'MISSING_CLIENT_SECRET_IN_PAYMENT_INTENT',
+};
 
 export class Config {
-	static appName = appName;
-	static accessToken = "";
-	static isProduction = production;
-	static appVersion = appVersion;
-	static appHeader = {
-		Accept: "application/json",
-		// "Content-Type": `multipart/form-data`,
-		appversion: this.appVersion.toLowerCase(),
-	};
+  static appName = appName;
+  static accessToken = '';
+  static isProduction = production;
+  static appVersion = appVersion;
+  static appHeader = {
+    Accept: 'application/json',
+    // "Content-Type": `multipart/form-data`,
+    appversion: this.appVersion.toLowerCase(),
+  };
 
-	static baseUrl = production ? liveHost : testHost;
-	static siteUrl = siteUrl;
-	static livekitServerUrl = livekitServerUrl;
-	static errorMessage = errorMessage;
-	static playStoreUrl = playStoreUrl;
+  // static baseUrl = production ? liveHost : testHost;
+  static baseUrl = 'http://10.0.2.2:3001/api/v1';
+  static siteUrl = siteUrl;
+  static livekitServerUrl = livekitServerUrl;
+  static errorMessage = errorMessage;
+  static playStoreUrl = playStoreUrl;
 }
--- src/screens/authModule/signIn/index.js
+++ src/screens/authModule/signIn/index.js
@@ -1,55 +1,208 @@
-import React, { useEffect, useState, useRef } from "react";
-import {
-    AppState,
-} from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Processing from "../../processing";
-import { CommonActions } from "@react-navigation/native";
-import apiRoutes from "../../../apis/apiRoutes";
-import axiosInstance from "../../../apis";
-import DeviceInfo from 'react-native-device-info';
-import { Config } from "../../../config";
-import { UpdateAppScreen } from "../../../components/UpdateScreen";
+import React, {useEffect, useState, useRef} from 'react';
+import {AppState, Image, StyleSheet, Text, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Processing from '../../processing';
+import {CommonActions, useNavigation} from '@react-navigation/native';
+import apiRoutes from '../../../apis/apiRoutes';
+import axiosInstance from '../../../apis';
+import {dynamicSize, showErrorMessage} from '../../../utils/helpers';
 
-const SignIn = ({ navigation }) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const appState = useRef(AppState.currentState);
+import MainContainer from '../../../components/mainContainer';
+import {loginScreen} from '../../../utils/resources';
+import {CommonStyle} from '../../../theme/style';
+import style from './style';
+import InputBox from '../../../components/inputBox';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Color} from '../../../theme/colors';
+import AppButton from '../../../components/appButton';
 
-    useEffect(() => {
-        checkTokenAndNavigate();
-    }, []);
+const SignIn = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+  const appState = useRef(AppState.currentState);
 
-    const checkTokenAndNavigate = async () => {
-        try {
-            // Verify if token is valid by making an API call
-            await AsyncStorage.clear();
-            const deviceId = await DeviceInfo.getUniqueId();
-            const response = await axiosInstance.post(apiRoutes.registerAndLoginForMobileApp, { mode: 'mobile', username: deviceId });
-            if (response.data.success) {
-				setIsLoading(false);
-                await AsyncStorage.setItem("access_token", response.data.token);
-                await AsyncStorage.setItem("user_info", JSON.stringify(response.data.user));
-                navigation.dispatch(
-                    CommonActions.reset({
-                        index: 1,
-                        routes: [{ name: "MatchList" }],
-                    })
-                );
-            } else {
-                // If token is invalid, remove it
-                await AsyncStorage.removeItem("access_token");
-            }
-        } catch (error) {
-            // If verification fails, remove token
-            await AsyncStorage.removeItem("access_token");
+  const [formData, setFormData] = useState({
+    mobileNo: '',
+  });
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    checkTokenAndNavigate();
+  }, []);
+
+  const checkTokenAndNavigate = async () => {
+    try {
+      const token = await AsyncStorage.getItem('access_token');
+      if (token) {
+        const response = await axiosInstance.get(apiRoutes.userInfo);
+
+        if (response.data.success) {
+          const today = new Date();
+          const expiresAtDate = new Date(response.data.user.expiresAt);
+
+          // Normalize both dates to midnight
+          today.setHours(0, 0, 0, 0);
+          expiresAtDate.setHours(0, 0, 0, 0);
+          // Check if the account is expired
+          await AsyncStorage.setItem(
+            'user_info',
+            JSON.stringify(response.data.user),
+          );
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{name: 'MatchList'}],
+            }),
+          );
+        } else {
+          // If token is invalid, remove it
+          await AsyncStorage.removeItem('access_token');
         }
-    };
+      }
+    } catch (error) {
+      // If verification fails, remove token
+      await AsyncStorage.removeItem('access_token');
+    }
+  };
 
-    return (
-        <>
-            {isLoading && <Processing />}
-        </>
-    );
+  const validateField = (name, value) => {
+    if (!value) {
+      return 'This field is required.';
+    }
+    return '';
+  };
+
+  const validateForm = () => {
+    let newErrors = {};
+    Object.keys(formData).forEach(key => {
+      if (key !== 'mode') {
+        const error = validateField(key, formData[key]);
+        if (error) newErrors[key] = error;
+      }
+    });
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleChange = (name, value) => {
+    setFormData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    // Validate field on change
+    const error = validateField(name, value);
+    setErrors(prev => ({
+      ...prev,
+      [name]: error,
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      onPressLogin(formData);
+    }
+  };
+
+  const onPressLogin = async data => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.post(apiRoutes.sigInWithPhone, data);
+      if (response.data.success) {
+        console.log('Login successful:', response.data);
+        await AsyncStorage.setItem('access_token', response.data.token);
+        await AsyncStorage.setItem(
+          'user_info',
+          JSON.stringify(response.data.user),
+        );
+        checkTokenAndNavigate();
+      } else {
+        showErrorMessage(
+          `Authentication failed! ${
+            response?.message ? response.message : 'Something went wrong.'
+          }`,
+        );
+      }
+    } catch (error) {
+      showErrorMessage(
+        `Authentication failed! ${
+          error?.message ? error.message : 'Something went wrong.'
+        }`,
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {isLoading && <Processing />}
+      {!isLoading && (
+        <MainContainer fluid>
+          <View style={style.lockIcon}>
+            <Image source={loginScreen.lockIcon} />
+            <Text
+              style={[
+                CommonStyle.appHeading,
+                {fontSize: dynamicSize(30), marginTop: dynamicSize(10, 1)},
+              ]}>
+              Enter your mobile number
+            </Text>
+          </View>
+          <View style={{paddingHorizontal: dynamicSize(5, 1)}}>
+            <View>
+              <View>
+                {errors?.mobileNo && (
+                  <Text style={CommonStyle.errorMsg}>{errors.username}</Text>
+                )}
+              </View>
+              <InputBox
+                placeholder={'+91 XXXX-XXXXXX'}
+                changeBorderColor={{
+                  borderColor: errors.mobileNo ? Color.primaryPink : null,
+                  borderWidth: errors.mobileNo ? 1 : null,
+                  marginTop: errors.mobileNo
+                    ? dynamicSize(0)
+                    : dynamicSize(20, 1),
+                }}
+                value={formData.mobileNo}
+                onChangeText={value => handleChange('mobileNo', value)}
+                keyboardType="number-pad"
+                showIcon={
+                  <Ionicons
+                    name="call-outline"
+                    size={20}
+                    color={Color.primaryBlue}
+                  />
+                }
+              />
+            </View>
+
+            <View style={CommonStyle.topSpacing}></View>
+            <AppButton
+              title={'Get Started'}
+              onPress={handleSubmit}
+              isLoading={isLoading}
+            />
+          </View>
+        </MainContainer>
+      )}
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    backgroundColor: '#FFDEE7',
+    borderRadius: 50,
+    padding: dynamicSize(5, 1),
+    marginBottom: dynamicSize(10, 1),
+  },
+  errorText: {color: 'red', fontSize: 12},
+  errorModal: {padding: 20, alignItems: 'center', backgroundColor: '#fff'},
+});
 
 export default SignIn;
